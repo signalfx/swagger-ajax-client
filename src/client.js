bc85330a129084f6fb699008d012736dde2a8c97
Ozan Turgut <ozanturgut@gmail.com>
2014-08-06 00:49:08 -0700

    Added travis build script

--- src/client.js
+++ src/client.js
@@ -20,34 +20,56 @@ var events = [
 ];
 
 function requestHandler(error, requestData){
-  if(error) throw error;
-  
-  var method = requestData.method;
-  var url = requestData.url;
-  var body = requestData.body;
-  var headers = requestData.headers;
+  return new Promise(function(resolve, reject){
+    if(error) return reject(error);
+    
+    var method = requestData.method;
+    var url = requestData.url;
+    var body = requestData.body;
+    var headers = requestData.headers;
 
-  var options = requestData.options;
-  var async = ('async' in options)? options.async : true;
-
-  var xhr = new XMLHttpRequest();
-  
-  xhr.open(method, url, async)
-  xhr.withCredentials = true;
-
-  if(headers){
-    Object.keys(headers).forEach(function(header){
-      xhr.setRequestHeader(header, headers[header]);
-    });
-  }
-
-  xhr.onloadend = function(){
-    if(this.status === 200){
-      resolve;
-    } else {
-      reject;
+    var options = requestData.options;
+    var async = ('async' in options)? options.async : true;
+    var withCredentials = ('withCredentials' in options)? options.withCredentials : false;
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open(method, url, async)
+    xhr.withCredentials = withCredentials;
+    xhr.timeout = 1;
+    if(headers){
+      Object.keys(headers).forEach(function(header){
+        xhr.setRequestHeader(header, headers[header]);
+      });
     }
-  };
 
-  xhr.send(body);
+    xhr.onabort = function(){
+      console.error('aborted', error, arguments);
+    };
+    xhr.onerror = function(){
+      console.error('error', error, arguments);
+    };
+    xhr.load = function(){};
+
+    xhr.timeout = function(){
+      console.error('timeout', error, arguments);
+    };
+
+    xhr.onloadend = function(){
+      var contentType = this.getResponseHeader('Content-Type');
+
+      var data = this.response;
+      if(contentType.indexOf('application/json') !== -1){
+        try {
+          data = JSON.parse(data);
+          resolve(data);
+        } catch(e){
+          reject(e, this.response);
+        }
+      } else {
+        resolve(this.response);
+      }
+    };
+
+    xhr.send(body);
+  });
 }
